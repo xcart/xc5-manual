f5e3f8bd344fa493bf0ec6ebae2762a912d58e11
Eugene Dementjev <daemos@x-cart.com>
2016-08-15 12:33:35 +0400

    Enable lightbox for pictures

---
+++ js/lightbox.js
@@ -0,0 +1,12 @@
+---
+layout: null
+---
+(function($){
+  $(document).ready(function(){
+    $('article img').wrap(function() {
+        return '<a class="lightbox-image" href="' + $(this).attr('src') + '">' + this.outerHTML + '</a>';
+    });
+
+    $('article .lightbox-image').fluidbox();
+  });
+})(jQuery);
