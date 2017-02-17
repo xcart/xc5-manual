8cc1759fa6d65f29ff7797afe3f37c114e2927b5
Eugene Dementjev <daemos@x-cart.com>
2017-02-17 10:59:42 +0400

    [!] Change ES host/port

--- js/elasticsearch.wrapper.js
+++ js/elasticsearch.wrapper.js
@@ -23,7 +23,7 @@ layout: null
   Search.prototype.connection = {
     host: {
       host: '{{ site.elasticsearch.js_host }}',
-      port: (window.location.protocol === 'https:' ? 8443 : 8000),
+      port: (window.location.protocol === 'https:' ? 443 : 80),
       protocol: (window.location.protocol === 'https:' ? 'https' : 'http'),
     },
     log:  'trace',
